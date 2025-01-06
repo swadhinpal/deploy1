8e4f84bcaeab4cf545466d72f6de036a251df51c
swadhinpal <bsse1302@iit.du.ac.bd>
2025-01-07 00:09:14 +0600

    test fails, should not deployed?12

--- test.js
+++ test.js
@@ -8,6 +8,6 @@ describe('GET /', () => {
   it('should return Hello World xyz', async () => {
     const response = await request(server).get('/');
     console.log(response.text);  // Optional, to view the response in the console
-    assert.strictEqual(response.text, 'Hello World');
+    assert.strictEqual(response.text, 'Hello World xyz');
   });
 });
