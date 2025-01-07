da9a37d8501ff05faa5426592c31261b54323567
swadhinpal <bsse1302@iit.du.ac.bd>
2025-01-07 09:46:38 +0600

    fail

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     try {
-        res.send('Hello World Pass'); 
+        res.send('Hello World Fail'); 
     } catch (err) {
         console.error("Error handling request:", err);
         res.status(500).send('Something went wrong');
